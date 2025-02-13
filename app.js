530e4b8b12bb8e0b1ea26e02e05021d05d94325e
Fitiavana Sambatra <Ghostrex2@gmail.com>
2025-02-13 19:53:42 +0300

    feat : create middleware

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 import express from 'express';
 import { PORT } from "./config/env.js";
+import userRouter from './routes/user.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+import authRouter from './routes/auth.routes.js';
 
 const app = express();
+const __version__ = "v1"
+
+app.use(`/api/${__version__}/auth`, authRouter);
+app.use(`/api/${__version__}/user`, userRouter);
+app.use(`/api/${__version__}/subscription`, subscriptionRouter);
 
 app.get('/', (req,res) => {
     res.send('Welcome to the Subscription Tracker API!');
