2ebb82d41aec7a910a05ac8ffac6d2266db5fe7a
Fitiavana Sambatra <Ghostrex2@gmail.com>
2025-02-19 20:33:30 +0300

    feat : get current user subscription

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import authorize from "../middlewares/auth.middleware.js";
-import { createSubscription } from "../controllers/subscription.controller.js";
+import { createSubscription, getUserSubscriptions } from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 
@@ -10,7 +10,7 @@ subscriptionRouter.post("/" , authorize , createSubscription);
 subscriptionRouter.put("/:id" , (req, res) => res.send({title : 'UPDATE subscription'}));
 subscriptionRouter.delete("/:id" , (req, res) => res.send({title : 'DELETE subscription'}));
 
-subscriptionRouter.get("/user/:id" , (req, res) => res.send({title : 'GET all subscriptions for user'}));
+subscriptionRouter.get("/user/:id" ,authorize, getUserSubscriptions);
 subscriptionRouter.put("/:id/cancel" , (req, res) => res.send({title : 'CANCEL subscriptions'}));
 subscriptionRouter.put("/upcoming-renewals" , (req, res) => res.send({title : 'GET upcoming renewals'}));
 
